fd59b9fac20ad936f26e890d4ad228e878d418fa
brenoafb <breno.afb@gmail.com>
2021-03-05 19:08:52 -0300

    Add tokenizer module

---
+++ lib/tokenizer.ex
@@ -0,0 +1,23 @@
+defmodule Tokenizer do
+  @moduledoc """
+  Tokenizer for parenthesized lists
+  """
+  @doc """
+  Tokenize a parenthesized list.
+
+  ## Examples
+
+    iex> "(hello (world))" |> Tokenizer.tokenize
+    ["(", "hello", "(", "world", ")", ")"]
+
+  """
+
+  def tokenize(input) do
+    special_chars
+    |> Enum.map(fn c -> {c, " " <> c <> " "} end)
+    |> Enum.reduce(input, fn {c, r}, acc -> String.replace(acc, c, r) end)
+    |> String.split
+  end
+
+  defp special_chars, do: ["(", ")", "'"]
+end
